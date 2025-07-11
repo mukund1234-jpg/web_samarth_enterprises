7c6f6d45a2cd347a71a64d36c80d8efc2b5e7594
mukundaparaj <mukundaparaj1@gmail.com>
2025-07-11 23:05:24 +0530

    add the error free cloudionary

--- AMCAPP/settings.py
+++ AMCAPP/settings.py
@@ -156,6 +156,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUD_NAME'),
-    'API_KEY': config('CLOUDINARY_API_KEY'),
-    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    'API_KEY': config('API_KEY'),
+    'API_SECRET': config('API_SECRET'),
 }
