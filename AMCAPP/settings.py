e444019c510e76594ab4c2ecc34454b6a67e8e17
mukundaparaj <mukundaparaj1@gmail.com>
2025-07-16 19:59:38 +0530

    static main and samarth-enterprises.up.railway.app

--- AMCAPP/settings.py
+++ AMCAPP/settings.py
@@ -114,9 +114,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),   # This includes your /static/ folder
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
