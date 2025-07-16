8b714286ca8130a59a540a4d646f8b53cf0888a4
mukundaparaj <mukundaparaj1@gmail.com>
2025-07-16 19:51:47 +0530

    static and samarth-enterprises.up.railway.app

--- AMCAPP/settings.py
+++ AMCAPP/settings.py
@@ -114,17 +114,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-# Media files (Uploaded images, videos, etc.)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+# Extra - Serve compressed static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
